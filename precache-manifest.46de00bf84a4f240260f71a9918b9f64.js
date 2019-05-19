bb12cb4c1967f86d797f55598cadc23ddd425b2e
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-05-19 12:52:28 +0200

    Updates

---
+++ precache-manifest.46de00bf84a4f240260f71a9918b9f64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29dfe1a2b58408ab6106b8865d3b7e42",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "58321edc8ff869c08d0f",
+    "url": "/robofriends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "f4d75ec7a7473da0d42d",
+    "url": "/robofriends/static/css/main.34cbca6d.chunk.css"
+  },
+  {
+    "revision": "58321edc8ff869c08d0f",
+    "url": "/robofriends/static/js/2.2d5add13.chunk.js"
+  },
+  {
+    "revision": "f4d75ec7a7473da0d42d",
+    "url": "/robofriends/static/js/main.8381d62f.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
